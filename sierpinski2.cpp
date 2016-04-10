97940807574e42035db5e4a8d41cec2ffb3035c1
angelynz95 <angel_lynz95@hotmail.com>
2016-04-10 20:55:26 +0700

    modify coloring for sierpinski with glTriangle

--- sierpinski2.cpp
+++ sierpinski2.cpp
@@ -3,12 +3,15 @@
 #include <cstdlib>
 #include <stdlib.h>
 
+#include <iostream>
+
 using namespace std;
 
 typedef GLfloat point2[2];
 
-point2 v[]={{-1.0, -0.58}, {1.0, -0.58}, {0.0, 1.15}};
+point2 v[]={{-2.0, -2.0}, {2.0, -2.0}, {0.0, 2.0}};
 int n;
+float reds[5], greens[5], blues[5];
 
 void myinit() {
 	glMatrixMode(GL_PROJECTION);
@@ -16,7 +19,6 @@ void myinit() {
 	gluOrtho2D(-2.0, 2.0, -2.0, 2.0);
 	glMatrixMode(GL_MODELVIEW);
 	glClearColor (1.0, 1.0, 1.0,1.0);
-	
 }
 
 void triangle( point2 a, point2 b, point2 c) {
@@ -37,31 +39,46 @@ void divide_triangle(point2 a, point2 b, point2 c, int m) {
 		divide_triangle(a, v0, v1, m-1);
 		divide_triangle(c, v1, v2, m-1);
 		divide_triangle(b, v2, v0, m-1);
+		glColor3f(reds[(n-m) % 5], greens[(n-m) % 5], blues[(n-m) % 5]);
+		triangle(v0, v1, v2);
 	}
 	else {
-		int v1 = rand() % 9 + 1;
-		float red = (float) v1 / 10;
-		int v2 = rand() % 9 + 1;
-		float green = (float) v2 / 10;
-		int v3 = rand() % 9 + 1;
-		float blue = (float) v3 / 10;
-		glColor3f(red, green, blue);
-
+		glColor3f(1, 0, 1);
 		triangle(a,b,c);
 	}
 }
 
+void randomizeColor() {
+	reds[0] = 0;
+	greens[0] = 0;
+	blues[0] = 1;
+	reds[1] = 1;
+	greens[1] = 0;
+	blues[1] = 0;
+	reds[2] = 0;
+	greens[2] = 1;
+	blues[2] = 0;
+	reds[3] = 1;
+	greens[3] = 1;
+	blues[3] = 1;
+	reds[4] = 1;
+	greens[4] = 1;
+	blues[4] = 0;
+}
+
 void display(void) {
 	glClear(GL_COLOR_BUFFER_BIT);
+	randomizeColor();
 	divide_triangle(v[0], v[1], v[2], n);
 	glFlush();
 }
 
 int main(int argc, char **argv) {
+	// best n = 5
 	n = atoi(argv[1]);
 	glutInit(&argc, argv);
 	glutInitDisplayMode(GLUT_SINGLE|GLUT_RGB);
-	glutInitWindowSize(500, 500);
+	glutInitWindowSize(600, 500);
 	glutCreateWindow("2D Gasket");
 	glutDisplayFunc(display);
 	myinit();
